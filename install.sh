6417a6cd66b3ab117d6305ddd4b39705e2f127d2
root <root@debTower>
2013-07-31 14:17:09 -0400

    renamed emacs to init.el. Helps with mode recognition. Also fixed install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="conkyrc emacs emacs.d xinitrc Xresources zshrc" # list of files/folders to symlink in homedir
+files="emacs.d xinitrc Xresources zshrc" # list of files/folders to symlink in homedir
 
 ##########
 
